ede0cc139b6de0658b29220a937a0329c0d15429
maaz-official <146274279+pwbcs0933@users.noreply.github.com>
2024-03-12 18:38:53 +0500

    changes in post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -56,43 +56,43 @@ export default function PostPage() {
         <Spinner size='xl' />
       </div>
     );
+
   return (
     <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
-      <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
-        {post && post.title}
-      </h1>
-      <Link
-        to={`/search?category=${post && post.category}`}
-        className='self-center mt-5'
-      >
-        <Button color='gray' pill size='xs'>
-          {post && post.category}
-        </Button>
-      </Link>
-      <img
-        src={post && post.image}
-        alt={post && post.title}
-        className='mt-10 p-3 max-h-[600px] w-full object-cover'
-      />
-      <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
-        <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-        <span className='italic'>
-          {post && (post.content.length / 1000).toFixed(0)} mins read
-        </span>
-      </div>
-      <div
-        className='p-3 max-w-2xl mx-auto w-full post-content'
-        dangerouslySetInnerHTML={{ __html: post && post.content }}
-      ></div>
-      <CommentSection postId={post._id} />
+      {error && <p className='text-red-500'>Error fetching post!</p>}
+      {post && (
+        <>
+          <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
+            {post.title}
+          </h1>
+          <Link to={`/search?category=${post.category}`} className='self-center mt-5'>
+            <Button color='gray' pill size='xs'>
+              {post.category}
+            </Button>
+          </Link>
+          <img src={post.image} alt={post.title} className='mt-10 p-3 max-h-[400px] w-full object-cover' />
+          <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
+            <span>{new Date(post.createdAt).toLocaleDateString()}</span>
+            <span className='italic'>{(post.content.length / 1000).toFixed(0)} mins read</span>
+          </div>
+          <div
+            className='p-3 max-w-2xl mx-auto w-full post-content'
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          ></div>
+          <CommentSection postId={post._id} />
+        </>
+      )}
 
-      <div className='flex flex-col justify-center items-center mb-5'>
-        <h1 className='text-xl mt-5'>Recent articles</h1>
-        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
-          {recentPosts &&
-            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+      {recentPosts && (
+        <div className='flex flex-col justify-center items-center mb-5'>
+          <h1 className='text-xl mt-5'>Recent articles</h1>
+          <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+            {recentPosts.map((post) => (
+              <PostCard key={post._id} post={post} />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </main>
   );
 }
